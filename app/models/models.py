b663c7850b6f61013ae0da5b0656bc8df882372d
omarnoubair <omar.nbr@gmail.com>
2021-07-26 14:31:27 +0000

    appli v1.0

--- app/models/models.py
+++ app/models/models.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 
 Base = automap_base()
-engine = create_engine('mysql+pymysql://root@localhost/projet_devops_2021', echo=False)
+engine = create_engine('mysql+pymysql://admin:Password123@rdstest2.cz3sp07bq1rp.eu-west-3.rds.amazonaws.com/projet_devops_2021', echo=False)
+
 Base.prepare(engine, reflect=True)
 
 Session = sessionmaker(bind=engine)
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, Flask
 from create_app import app
 from services.connexion import *
 import json
