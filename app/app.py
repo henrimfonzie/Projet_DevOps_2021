580db0a7a2f4f269d6423af338d7ac92bb05ab02
noubaio <36126062+noubaio@users.noreply.github.com>
2021-08-05 14:48:39 +0200

    flask log

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from create_app import app
 import routes
+import logging
+from datetime import date
 
-
+today = date.today()
 if __name__ == '__main__':
+    logfile=str(today)+".log"
+    logging.basicConfig(filename=logfile,level=logging.DEBUG)
     app.run(host="0.0.0.0", debug=True)
