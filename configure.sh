d5b8944e6c4359fa3b52198ccd8e7ffedbde58b9
omarnoubair <omar.nbr@gmail.com>
2021-09-20 13:33:30 +0000

    config update

--- configure.sh
+++ configure.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
- 
-sudo apt install jq unzip mysql-server git -y
+
+apt-get update
+
+apt-get install curl jq unzip mysql-server python3 git -y
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+./aws/install
