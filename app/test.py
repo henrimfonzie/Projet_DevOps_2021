84798bfaa0bd12b5793174f78fcfeb3b322ba672
omarnoubair <omar.nbr@gmail.com>
2021-09-23 06:46:09 +0000

    last commit

--- app/test.py
+++ app/test.py
@@ -1,14 +1,19 @@
 import unittest
-import configparser
 
 from services.connexion import *
 class TestStringMethods(unittest.TestCase):
-    def testuser(self):
-        if session.query(Utilisateur).filter_by(mail = "hmfonzie@yahoo.com", motdepass ="root").first() != None:
+    
+    def database_co(self):
+        if session.query(Utilisateur).filter_by(mail = "admin", motdepass ="root").first() != None:
             test = True
         else:
             test = False
         self.assertTrue(test)
-
+    def testuser(self):
+        if session.query(Utilisateur).filter_by(mail = "admin", motdepass ="root").first() != None:
+            test = True
+        else:
+            test = False
+        self.assertTrue(test)
 if __name__ == '__main__':
     unittest.main()
